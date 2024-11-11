0a15523e107fd265bea92f742198eac3c6eb67ce
MurilloSSJ <77257498+MurilloSSJ@users.noreply.github.com>
2024-11-11 09:52:32 -0300

    remove airflow unnused code, and create core in golang

---
+++ lessflow/core/internal/config/config.model.go
@@ -0,0 +1,11 @@
+package config
+
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Database string `yaml:"database"`
+	} `yaml:"database"`
+}
